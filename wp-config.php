b5bc85477e0c05d9062729d0d10d2e1e34dbfe7c
elmehdiboutaleb <boutaleb.em@gmail.com>
2015-12-02 11:43:49 +0100

    Update DB config

--- wp-config.php
+++ wp-config.php
@@ -5,37 +5,39 @@
  * Le script de création wp-config.php utilise ce fichier lors de l'installation.
  * Vous n'avez pas à utiliser l'interface web, vous pouvez directement
  * renommer ce fichier en "wp-config.php" et remplir les variables à la main.
- * 
+ *
  * Ce fichier contient les configurations suivantes :
- * 
+ *
  * * réglages MySQL ;
  * * clefs secrètes ;
  * * préfixe de tables de la base de données ;
  * * ABSPATH.
- * 
- * @link https://codex.wordpress.org/Editing_wp-config.php 
- * 
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
  * @package WordPress
  */
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
+
+/*
 define('DB_NAME', 'wpmj');
-
-/** Utilisateur de la base de données MySQL. */
 define('DB_USER', 'root');
-
-/** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', '');
-
-/** Adresse de l'hébergement MySQL. */
 define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8mb4');
+*/
 
-/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define('DB_NAME', 'elmehdibrhcntct');
+define('DB_USER', 'elmehdibrhcntct');
+define('DB_PASSWORD', 'mJ_2015#');
+define('DB_HOST', 'db4free.net');
 define('DB_CHARSET', 'utf8mb4');
 
-/** Type de collation de la base de données. 
-  * N'y touchez que si vous savez ce que vous faites. 
+
+/** Type de collation de la base de données.
+  * N'y touchez que si vous savez ce que vous faites.
   */
 define('DB_COLLATE', '');
 
@@ -43,7 +45,7 @@ define('DB_COLLATE', '');
  * Clefs uniques d'authentification et salage.
  *
  * Remplacez les valeurs par défaut par des phrases uniques !
- * Vous pouvez générer des phrases aléatoires en utilisant 
+ * Vous pouvez générer des phrases aléatoires en utilisant
  * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clefs secrètes de WordPress.org}.
  * Vous pouvez modifier ces phrases à n'importe quel moment, afin d'invalider tous les cookies existants.
  * Cela forcera également tous les utilisateurs à se reconnecter.
@@ -64,26 +66,26 @@ define('NONCE_SALT',       'UO:Nx+ox1Ieb$Y(q=ZA$ujhgD~NQ0!{,-`0Fb|R3 oOyet@#f`|}
  * Préfixe de base de données pour les tables de WordPress.
  *
  * Vous pouvez installer plusieurs WordPress sur une seule base de données
- * si vous leur donnez chacune un préfixe unique. 
+ * si vous leur donnez chacune un préfixe unique.
  * N'utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés!
  */
 $table_prefix  = 'wp_mj';
 
-/** 
+/**
  * Pour les développeurs : le mode déboguage de WordPress.
- * 
+ *
  * En passant la valeur suivante à "true", vous activez l'affichage des
  * notifications d'erreurs pendant votre essais.
  * Il est fortemment recommandé que les développeurs d'extensions et
- * de thèmes se servent de WP_DEBUG dans leur environnement de 
+ * de thèmes se servent de WP_DEBUG dans leur environnement de
  * développement.
- * 
- * Pour obtenir plus d'information sur les constantes 
+ *
+ * Pour obtenir plus d'information sur les constantes
  * qui peuvent être utilisée pour le déboguage, consultez le Codex.
- * 
- * @link https://codex.wordpress.org/Debugging_in_WordPress 
- */ 
-define('WP_DEBUG', false); 
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
 
 /* C'est tout, ne touchez pas à ce qui suit ! Bon blogging ! */
 
